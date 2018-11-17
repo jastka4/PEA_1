267715e962ad1eef1bf27bd7d91d551d5bdaecda
Justyna Skalska <justyna_skalska@o2.pl>
2018-11-17 14:01:33 +0100

    Add file not found exception

---
+++ include/FileNotFoundException.h
@@ -0,0 +1,20 @@
+//
+// Created by jskalska on 12/11/2018.
+//
+
+#pragma once
+#include <stdexcept>
+
+class FileNotFoundException :
+        public std::runtime_error
+{
+    const char* file;
+public:
+    FileNotFoundException(const char* msg, const char* _file): runtime_error(msg),
+                                                               file(_file) {}
+
+    const char* getFile() const
+    {
+        return file;
+    }
+};
